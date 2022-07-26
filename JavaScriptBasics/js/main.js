fe49a02e9a53933281f20b6a48d1e7093b3de769
Yazima <yazima04200@gmail.com>
2022-07-26 17:54:00 +0900

    switch条件分岐まで追記

--- JavaScriptBasics/js/main.js
+++ JavaScriptBasics/js/main.js
@@ -81,8 +81,101 @@ console.log(typeof undefined);
 console.log(typeof null); // ブラウザツール上ではobjectと表示されるが有名なバグなのでこれでOK
 */
 
+/*
 console.log('5' * 3);
 console.log('5' - '3');
 console.log('5' + 3); // +を使用した場合は文字列として認識されるため文字として53となる
 console.log(parseInt('5', 10) + 3); // どうしても計算がしたい場合はparseIntを使用すれば計算ができる
 console.log(parseInt('hello', 10)); // ’’内に数値を使用しなかった場合、NaNという表示が出る
+*/
+
+/* 比較演算
+const price = 1200;
+
+console.log(price > 1000); // true　～より大きい
+console.log(price < 1000); // false　～より小さい
+console.log(price >= 1000); // true　～以上
+console.log(price <= 1000); // false　～以下
+console.log(price === 1000); // false　～と等しい
+console.log(price !== 1000); // true　～と異なる
+
+// false <- 0, null, undefined, '', false
+// true <- それ以外
+
+console.log(Boolean(0));
+console.log(Boolean('hello'));
+*/
+
+// if
+/*
+const score = 70;
+
+if (score >= 80) {
+  console.log('Great!');
+} else if (score >= 60) {
+  console.log('Good.');
+} else {
+  console.log('OK...');
+}
+*/
+
+// 条件演算
+/*
+const score = 85;
+
+if (score >= 80) {
+  console.log('Great!');
+} else {
+  console.log('OK...');
+}
+
+score >= 80 ? console.log('Great!') : console.log('OK...'); // 上記と同じ結果が出る
+*/
+
+// 論理演算
+/*
+const score = 60;
+const name = 'taguchi';
+
+if (score >= 50) {
+  if (name === 'taguchi') {
+    console.log('Good job!');
+  }
+}
+
+// && なおかつ（AND）
+// || もしくは（OR）
+// ! ～ではない（NOT）
+
+if (score >= 50 && name === 'taguchi') {
+  console.log('Good job!');
+}   //上記と同じ結果が出る
+*/
+
+// switch
+
+const signal = 'red';
+
+// if (signal === 'red') {
+//   console.log('Stop!');
+// } else if (signal === 'yellow') {
+//   console.log('Caution!');
+// } else if (signal === 'blue') {
+//   console.log('Go!');
+// }
+
+switch (signal) {
+  case 'red':
+    console.log('Stop!');
+    break;
+  case 'yellow':
+    console.log('Caution!');
+    break;
+  case 'blue':
+  case 'green': // 青または緑にしたい場合はこの行の通り緑追加
+    console.log('Go!');
+    break;
+  default:
+    console.log('Wrong signal!');
+    break;
+}
